86531b8d973e88374b046ac713565def173db527
zhoukunpeng504 <18749679769@163.com>
2018-06-25 15:58:25 +0800

    init project

--- scripts/aa/include.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-/*ad0c3*/
-
-
-/*ad0c3*/
-
-
-echo @file_get_contents('index.html.bak.bak');
--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
 #coding:utf-8
 # write  by  zhou
+from setuptools import  setup,find_packages
+setup(
+    name="cap-python",
+    packages=list(set(list(find_packages())+["twisted.plugins"])),
+    version='0.0.1',
+    install_requires=["setuptools",
+                      "MySQL-python",
+                      "twisted==15.3.0",
+                      "txscheduling",
+                      "psutil",
+                      "django==1.4.22"],
+    include_package_data=True
+)
